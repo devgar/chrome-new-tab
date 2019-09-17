70f213e7db6aa6e448383018b84152688491c8d5
Edgar Albalate <dev.gardo@gmail.com>
2019-09-17 10:49:39 +0200

    Initial commit

---
+++ src/Video.svelte
@@ -0,0 +1,29 @@
+<script>
+  let visible = false;
+  let video;
+</script>
+
+<video class:visible autoplay loop on:loadeddata="{e => visible = true}">
+  <source src="ri.mp4" type="video/mp4" tabindex="-1">  
+</video>
+
+<style>
+video {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  opacity: 0;
+  clip-path: circle(1vw at 50% 50%);
+  transform: translate(-50%, -50%);
+  min-width: 100vw;
+  min-height: 100vh;
+  pointer-events: none;
+  z-index: -1;
+}
+
+video.visible {
+  transition: opacity 0.1s, clip-path 0.7s;
+  clip-path: circle(100vw at 50% 50%);
+  opacity: 1;
+}
+</style>
